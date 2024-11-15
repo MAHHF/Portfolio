41b32d950c8aa6f6bb6d527cad32aa25a272f129
MAHHF <mahf@outlook.de>
2024-11-15 23:18:31 +0100

    Create script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+function toggleMenu() {
+    var menu = document.getElementById('menu');
+    menu.classList.toggle('open');  // Menü ein- oder ausblenden
+
+    var hamburger = document.querySelector('.hamburger-menu');
+    hamburger.classList.toggle('open');  // Animation für die Linie
+}
