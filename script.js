0c9a3dd60dc8e124fa45967e8f83920e27fac0c5
MAHHF <mahf@outlook.de>
2024-11-15 23:56:59 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// Funktion zum Umschalten des Menüs
+// Toggle für Hamburger-Menü
 function toggleMenu() {
     var menu = document.getElementById('menu');
     menu.classList.toggle('open');  // Menü ein- oder ausblenden
@@ -6,3 +6,6 @@ function toggleMenu() {
     var hamburger = document.querySelector('.hamburger-menu');
     hamburger.classList.toggle('open');  // Animation für die Linie
 }
+
+// Event Listener für das Hamburger-Menü
+document.querySelector('.hamburger-menu').addEventListener('click', toggleMenu);
