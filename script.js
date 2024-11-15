f6446b90967b83cd128be76d9971a18e166e982b
MAHHF <mahf@outlook.de>
2024-11-15 23:25:55 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 function toggleMenu() {
+    // Menü-Element (mit ID "menu")
     var menu = document.getElementById('menu');
     menu.classList.toggle('open');  // Menü ein- oder ausblenden
 
+    // Hamburger-Menü (mit der Klasse "hamburger-menu")
     var hamburger = document.querySelector('.hamburger-menu');
     hamburger.classList.toggle('open');  // Animation für die Linie
 }
