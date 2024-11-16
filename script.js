346d10c38c2811f298c192fc05c0368a8aad25e9
MAHHF <mahf@outlook.de>
2024-11-16 23:58:22 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
+// Warten, bis das DOM geladen ist
+document.addEventListener('DOMContentLoaded', function () {
+    // Alle Links im Navigationsmenü finden
+    const navLinks = document.querySelectorAll('nav a');
 
+    // Funktion zum Hinzufügen und Entfernen der 'active' Klasse
+    navLinks.forEach(function(link) {
+        link.addEventListener('click', function(event) {
+            // Entfernt die 'active' Klasse von allen Links
+            navLinks.forEach(function(link) {
+                link.classList.remove('active');
+            });
+
+            // Fügt die 'active' Klasse dem angeklickten Link hinzu
+            event.target.classList.add('active');
+        });
+    });
+});
