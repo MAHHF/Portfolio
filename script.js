d16f4d6e0c2c69639527371df0b0d2f91a36e37d
MAHHF <mahf@outlook.de>
2024-11-17 13:01:27 +0100

    Update script.js

--- script.js
+++ script.js
@@ -4,26 +4,22 @@ document.addEventListener("DOMContentLoaded", function() {
 
     // Überprüfen, ob wir uns auf der "photo"-Seite befinden
     if (bodyId === "photo") {
-        // Der Link für "Photo" wird zu "35mm" geändert
         document.querySelector('a[href="photo.html"]').textContent = "35mm";
     }
 
     // Überprüfen, ob wir uns auf der "mahf"-Seite befinden
     if (bodyId === "mahf") {
-        // Der Link für "About" wird zu "MAHF" geändert
         document.querySelector('a[href="mahf.html"]').textContent = "MAHF";
     }
 
     // Überprüfen, ob wir uns auf der "home"-Seite befinden
     if (bodyId === "home") {
-        // Der Link für "Home" wird zu "Zuhause" geändert
         document.querySelector('a[href="index.html"]').textContent = "Zuhause";
     }
 
     // Füge den aktiven Link zur Navigation hinzu
     var links = document.querySelectorAll('nav a');
     links.forEach(function(link) {
-        // Wenn der Link der aktuellen Seite entspricht, wird er mit 'active' markiert
         if (link.href.includes(bodyId)) {
             link.classList.add('active');
         } else {
